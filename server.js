9f11604f536b87e771de3dd1642b41c85c79c568
Robby Roda <robbyroda.00@gmail.com>
2025-08-18 22:55:17 +0800

    asdasd

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ import "dotenv/config";
 import admin from "firebase-admin";
 
 /* ------------------ FIREBASE INIT ------------------ */
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
-  });
-}
-const db = admin.firestore();
+var admin = require("firebase-admin");
+
+var serviceAccount = require("fb.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 /* ------------------ EXPRESS INIT ------------------ */
 const app = express();
